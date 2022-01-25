70c6eb1a1f5d78527ff5661757abe2a53608de53
shi-n <shi-n_xyz@cloudsquare.jp>
2022-01-26 06:30:07 +0900

    Initial Commit

---
+++ FDMUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FDMUI
+//
+//  Created by Shin Inaba on 2022/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FDMUI/FDMUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FDMUIApp.swift
+//  FDMUI
+//
+//  Created by Shin Inaba on 2022/01/26.
+//
+
+import SwiftUI
+
+@main
+struct FDMUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
