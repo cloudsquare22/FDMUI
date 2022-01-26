abd4613328b699f46913652592f819906ac5046c
shi-n <shi-n_xyz@cloudsquare.jp>
2022-01-27 06:52:54 +0900

    add value change & change value type.

--- FDMUI/MainView.swift
+++ FDMUI/MainView.swift
@@ -8,19 +8,49 @@
 import SwiftUI
 
 struct MainView: View {
-    @State var line: String = ""
+    @State var step: Double = 0
+    @State var productivity: Double = 0
+    @State var fd: Double = 0
+    @State var dd: Double = 0
+    @State var cd: Double = 0
+    @State var ut: Double = 0
+    @State var it: Double = 0
+
     var body: some View {
         VStack {
+            Label("FDMUI", systemImage: "chart.xyaxis.line")
+                .font(.largeTitle)
             HStack {
-                LabelTextFieldView(input: self.$line, title: "Step", systemImage: "doc.plaintext")
+                LabelTextFieldView(input: self.$step, title: "Step", systemImage: "doc.plaintext", unit: "KL")
+                    .onChange(of: self.step, perform: { newline in
+                        print("change:" + newline)
+                    })
+                LabelTextFieldView(input: self.$step, title: "Productivity", systemImage: "doc.plaintext")
+                    .onChange(of: self.productivity, perform: { newline in
+                        print("change:" + newline)
+                    })
             }
             HStack {
-                LabelTextFieldView(input: self.$line, title: "FD", systemImage: "doc.plaintext")
-                LabelTextFieldView(input: self.$line, title: "DD", systemImage: "doc.plaintext")
-                LabelTextFieldView(input: self.$line, title: "CD", systemImage: "doc.plaintext")
-                LabelTextFieldView(input: self.$line, title: "UT", systemImage: "doc.plaintext")
-                LabelTextFieldView(input: self.$line, title: "IT", systemImage: "doc.plaintext")
-
+                LabelTextFieldView(input: self.$fd, title: "FD", systemImage: "doc.plaintext")
+                    .onChange(of: self.fd, perform: { newline in
+                        print("change:" + newline)
+                    })
+                LabelTextFieldView(input: self.$dd, title: "DD", systemImage: "doc.plaintext")
+                    .onChange(of: self.dd, perform: { newline in
+                        print("change:" + newline)
+                    })
+                LabelTextFieldView(input: self.$cd, title: "CD", systemImage: "doc.plaintext")
+                    .onChange(of: self.cd, perform: { newline in
+                        print("change:" + newline)
+                    })
+                LabelTextFieldView(input: self.$ut, title: "UT", systemImage: "doc.plaintext")
+                    .onChange(of: self.ut, perform: { newline in
+                        print("change:" + newline)
+                    })
+                LabelTextFieldView(input: self.$it, title: "IT", systemImage: "doc.plaintext")
+                    .onChange(of: self.it, perform: { newline in
+                        print("change:" + newline)
+                    })
             }
             Spacer()
         }
@@ -36,15 +66,22 @@ struct MainView_Previews: PreviewProvider {
 
 
 struct LabelTextFieldView: View {
-    @Binding var input: String
+    @Binding var input: Double
     var title: String
     var systemImage: String
+    var unit: String? = nil
     
     var body: some View {
         VStack(alignment: .leading, spacing: 8.0) {
             Label(self.title, systemImage: self.systemImage)
-            TextField("Number", text: self.$input)
-                .keyboardType(UIKeyboardType.numberPad)
+            HStack {
+                TextField("Number", text: self.$input)
+                    .keyboardType(UIKeyboardType.numberPad)
+                    .textFieldStyle(.roundedBorder)
+                if let unit = self.unit {
+                    Text(unit)
+                }
+            }
         }
         .padding(8.0)
         .overlay(
