d388156852bb214679a5902829579dc474edee78
shi-n <shi-n_xyz@cloudsquare.jp>
2022-01-29 08:35:41 +0900

    人調整追加、デザイン調整、データ保存と読出

--- FDMUI/DataModel.swift
+++ FDMUI/DataModel.swift
@@ -22,6 +22,75 @@ final class DataModel: ObservableObject {
     @Published var adjustut: Double = 0.0
     @Published var adjustit: Double = 0.0
     @Published var adjustsum: Double = 0.0
+    @Published var personfd: Double = 0.0
+    @Published var persondd: Double = 0.0
+    @Published var personcd: Double = 0.0
+    @Published var personut: Double = 0.0
+    @Published var personit: Double = 0.0
+    @Published var personsum: Double = 0.0
+
+    let userdefault = UserDefaults.standard
+    
+    init() {
+        self.load()
+        self.calculation()
+        self.adjustcalculation()
+        self.personcalculation()
+    }
+
+    func load() {
+        if let step = userdefault.object(forKey: "step") as? Double {
+            self.step = step
+        }
+        if let productivity = userdefault.object(forKey: "productivity") as? Double {
+            self.productivity = productivity
+        }
+        if let adjustfd = userdefault.object(forKey: "adjustfd") as? Double {
+            self.adjustfd = adjustfd
+        }
+        if let adjustdd = userdefault.object(forKey: "adjustdd") as? Double {
+            self.adjustdd = adjustdd
+        }
+        if let adjustcd = userdefault.object(forKey: "adjustcd") as? Double {
+            self.adjustcd = adjustcd
+        }
+        if let adjustut = userdefault.object(forKey: "adjustut") as? Double {
+            self.adjustut = adjustut
+        }
+        if let adjustit = userdefault.object(forKey: "adjustit") as? Double {
+            self.adjustit = adjustit
+        }
+        if let personfd = userdefault.object(forKey: "personfd") as? Double {
+            self.personfd = personfd
+        }
+        if let persondd = userdefault.object(forKey: "persondd") as? Double {
+            self.persondd = persondd
+        }
+        if let personcd = userdefault.object(forKey: "personcd") as? Double {
+            self.personcd = personcd
+        }
+        if let personut = userdefault.object(forKey: "personut") as? Double {
+            self.personut = personut
+        }
+        if let personit = userdefault.object(forKey: "personit") as? Double {
+            self.personit = personit
+        }
+    }
+    
+    func save() {
+        self.userdefault.set(self.step, forKey: "step")
+        self.userdefault.set(self.productivity, forKey: "productivity")
+        self.userdefault.set(self.adjustfd, forKey: "adjustfd")
+        self.userdefault.set(self.adjustdd, forKey: "adjustdd")
+        self.userdefault.set(self.adjustcd, forKey: "adjustcd")
+        self.userdefault.set(self.adjustut, forKey: "adjustut")
+        self.userdefault.set(self.adjustit, forKey: "adjustit")
+        self.userdefault.set(self.personfd, forKey: "personfd")
+        self.userdefault.set(self.persondd, forKey: "persondd")
+        self.userdefault.set(self.personcd, forKey: "personcd")
+        self.userdefault.set(self.personut, forKey: "personut")
+        self.userdefault.set(self.personit, forKey: "personit")
+    }
 
     func calculation() {
         self.manmonth = self.step / self.productivity
@@ -34,6 +103,10 @@ final class DataModel: ObservableObject {
     
     func adjustcalculation() {
         self.adjustsum = self.adjustfd + self.adjustdd + self.adjustcd + self.adjustut + self.adjustit
+    }
+
+    func personcalculation() {
+        self.personsum = self.personfd + self.persondd + self.personcd + self.personut + self.personit
     }
 }
 
--- FDMUI/MainView.swift
+++ FDMUI/MainView.swift
@@ -9,97 +9,48 @@ import SwiftUI
 
 struct MainView: View {
     @EnvironmentObject var dataModel: DataModel
-
+    @Environment(\.scenePhase) private var scenePhase
+    
     var body: some View {
         VStack(spacing: 8.0) {
             Label("FDMUI", systemImage: "chart.xyaxis.line")
                 .font(.largeTitle)
-            HStack {
-                LabelTextFieldNumberView(input: self.$dataModel.step, title: "Step(KL)", systemImage: "doc.plaintext")
-                    .onChange(of: self.dataModel.step, perform: { newline in
-                        print("change:\(newline)")
-                        self.dataModel.calculation()
-                    })
-                LabelTextFieldNumberView(input: self.$dataModel.productivity, title: "Productivity", systemImage: "doc.plaintext")
-                    .onChange(of: self.dataModel.productivity, perform: { newline in
-                        print("change:\(newline)")
-                        self.dataModel.calculation()
-                    })
-                LabelTextFieldNumberView(input: self.$dataModel.manmonth, title: "Man-Month", systemImage: "doc.plaintext")
-                    .onChange(of: self.dataModel.manmonth, perform: { newline in
-                        print("change:\(newline)")
-                        self.dataModel.calculation()
-                    })
-            }
-            HStack {
-                LabelTextFieldNumberView(input: self.$dataModel.fd, title: "FD", systemImage: "doc.plaintext")
-                    .onChange(of: self.dataModel.fd, perform: { newline in
-                        print("change:\(newline)")
-                    })
-                LabelTextFieldNumberView(input: self.$dataModel.dd, title: "DD", systemImage: "doc.plaintext")
-                    .onChange(of: self.dataModel.dd, perform: { newline in
-                        print("change:\(newline)")
-                    })
-                LabelTextFieldNumberView(input: self.$dataModel.cd, title: "CD", systemImage: "doc.plaintext")
-                    .onChange(of: self.dataModel.cd, perform: { newline in
-                        print("change:\(newline)")
-                    })
-                LabelTextFieldNumberView(input: self.$dataModel.ut, title: "UT", systemImage: "doc.plaintext")
-                    .onChange(of: self.dataModel.ut, perform: { newline in
-                        print("change:\(newline)")
-                    })
-                LabelTextFieldNumberView(input: self.$dataModel.it, title: "IT", systemImage: "doc.plaintext")
-                    .onChange(of: self.dataModel.it, perform: { newline in
-                        print("change:\(newline)")
-                    })
-            }
-            Image(systemName: "arrowtriangle.down")
-                .font(.largeTitle)
-            HStack {
-                Label("Adjust", systemImage: "digitalcrown.horizontal.arrow.counterclockwise")
-                    .font(.largeTitle)
-                Spacer()
-                if self.dataModel.manmonth == self.dataModel.adjustsum {
-                    Text(String.init(format: "Sum:%.2f", self.dataModel.adjustsum))
-                        .font(.largeTitle)
-                }
-                else {
-                    Text(String.init(format: "Sum:%.2f", self.dataModel.adjustsum))
-                        .font(.largeTitle)
-                        .foregroundColor(.red)
-                }
-            }
-            HStack {
-                LabelTextFieldNumberView(input: self.$dataModel.adjustfd, title: "FD", systemImage: "doc.plaintext")
-                    .onChange(of: self.dataModel.adjustfd, perform: { newline in
-                        print("change:\(newline)")
-                        self.dataModel.adjustcalculation()
-                    })
-                LabelTextFieldNumberView(input: self.$dataModel.adjustdd, title: "DD", systemImage: "doc.plaintext")
-                    .onChange(of: self.dataModel.adjustdd, perform: { newline in
-                        print("change:\(newline)")
-                        self.dataModel.adjustcalculation()
-                    })
-                LabelTextFieldNumberView(input: self.$dataModel.adjustcd, title: "CD", systemImage: "doc.plaintext")
-                    .onChange(of: self.dataModel.adjustcd, perform: { newline in
-                        print("change:\(newline)")
-                        self.dataModel.adjustcalculation()
-                    })
-                LabelTextFieldNumberView(input: self.$dataModel.adjustut, title: "UT", systemImage: "doc.plaintext")
-                    .onChange(of: self.dataModel.adjustut, perform: { newline in
-                        print("change:\(newline)")
-                        self.dataModel.adjustcalculation()
-                    })
-                LabelTextFieldNumberView(input: self.$dataModel.adjustit, title: "IT", systemImage: "doc.plaintext")
-                    .onChange(of: self.dataModel.adjustit, perform: { newline in
-                        print("change:\(newline)")
-                        self.dataModel.adjustcalculation()
-                    })
-            }
+            BaseView()
+                .padding(8.0)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(Color.primary, lineWidth: 1)
+                )
+//            Image(systemName: "arrowtriangle.down")
+//                .font(.largeTitle)
+            AdjustView()
+                .padding(8.0)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(Color.primary, lineWidth: 1)
+                )
+            PersonView()
+                .padding(8.0)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(Color.primary, lineWidth: 1)
+                )
             Spacer()
         }
         .padding(8.0)
-    }
+        .onChange(of: scenePhase, perform: { value in
+                                    switch(value) {
+                                    case .active:
+                                        print("active")
+                                    case .background:
+                                        print("background")
+                                        self.dataModel.save()
+                                    case .inactive:
+                                        print("inactive")
+                                    @unknown default:
+                                        print("default")
+                                    }
+                                })    }
 }
 
 struct MainView_Previews: PreviewProvider {
@@ -163,5 +114,158 @@ struct LabelTextFieldNumberView: View {
             RoundedRectangle(cornerRadius: 8)
                 .stroke(Color.primary, lineWidth: 1)
         )
+    }
+}
+
+struct BaseView: View {
+    @EnvironmentObject var dataModel: DataModel
+    var body: some View {
+        VStack {
+            HStack {
+                Label("Base", systemImage: "doc.on.doc")
+                    .font(.largeTitle)
+                Spacer()
+            }
+            HStack {
+                LabelTextFieldNumberView(input: self.$dataModel.step, title: "Step(KL)", systemImage: "doc.plaintext")
+                    .onChange(of: self.dataModel.step, perform: { newline in
+                        print("change:\(newline)")
+                        self.dataModel.calculation()
+                    })
+                LabelTextFieldNumberView(input: self.$dataModel.productivity, title: "Productivity", systemImage: "doc.plaintext")
+                    .onChange(of: self.dataModel.productivity, perform: { newline in
+                        print("change:\(newline)")
+                        self.dataModel.calculation()
+                    })
+                LabelTextFieldNumberView(input: self.$dataModel.manmonth, title: "Man-Month", systemImage: "doc.plaintext")
+                    .onChange(of: self.dataModel.manmonth, perform: { newline in
+                        print("change:\(newline)")
+                        self.dataModel.calculation()
+                    })
+            }
+            HStack {
+                LabelTextFieldNumberView(input: self.$dataModel.fd, title: "FD", systemImage: "doc.plaintext")
+                    .onChange(of: self.dataModel.fd, perform: { newline in
+                        print("change:\(newline)")
+                    })
+                LabelTextFieldNumberView(input: self.$dataModel.dd, title: "DD", systemImage: "doc.plaintext")
+                    .onChange(of: self.dataModel.dd, perform: { newline in
+                        print("change:\(newline)")
+                    })
+                LabelTextFieldNumberView(input: self.$dataModel.cd, title: "CD", systemImage: "doc.plaintext")
+                    .onChange(of: self.dataModel.cd, perform: { newline in
+                        print("change:\(newline)")
+                    })
+                LabelTextFieldNumberView(input: self.$dataModel.ut, title: "UT", systemImage: "doc.plaintext")
+                    .onChange(of: self.dataModel.ut, perform: { newline in
+                        print("change:\(newline)")
+                    })
+                LabelTextFieldNumberView(input: self.$dataModel.it, title: "IT", systemImage: "doc.plaintext")
+                    .onChange(of: self.dataModel.it, perform: { newline in
+                        print("change:\(newline)")
+                    })
+            }
+        }
+    }
+}
+
+struct AdjustView: View {
+    @EnvironmentObject var dataModel: DataModel
+
+    var body: some View {
+        VStack {
+            HStack {
+                Label("Adjust", systemImage: "digitalcrown.horizontal.arrow.counterclockwise")
+                    .font(.largeTitle)
+                Spacer()
+                if self.dataModel.manmonth == self.dataModel.adjustsum {
+                    Text(String.init(format: "Sum:%.2f", self.dataModel.adjustsum))
+                        .font(.largeTitle)
+                }
+                else {
+                    Text(String.init(format: "Sum:%.2f", self.dataModel.adjustsum))
+                        .font(.largeTitle)
+                        .foregroundColor(.red)
+                }
+            }
+            HStack {
+                LabelTextFieldNumberView(input: self.$dataModel.adjustfd, title: "FD", systemImage: "doc.plaintext")
+                    .onChange(of: self.dataModel.adjustfd, perform: { newline in
+                        print("change:\(newline)")
+                        self.dataModel.adjustcalculation()
+                    })
+                LabelTextFieldNumberView(input: self.$dataModel.adjustdd, title: "DD", systemImage: "doc.plaintext")
+                    .onChange(of: self.dataModel.adjustdd, perform: { newline in
+                        print("change:\(newline)")
+                        self.dataModel.adjustcalculation()
+                    })
+                LabelTextFieldNumberView(input: self.$dataModel.adjustcd, title: "CD", systemImage: "doc.plaintext")
+                    .onChange(of: self.dataModel.adjustcd, perform: { newline in
+                        print("change:\(newline)")
+                        self.dataModel.adjustcalculation()
+                    })
+                LabelTextFieldNumberView(input: self.$dataModel.adjustut, title: "UT", systemImage: "doc.plaintext")
+                    .onChange(of: self.dataModel.adjustut, perform: { newline in
+                        print("change:\(newline)")
+                        self.dataModel.adjustcalculation()
+                    })
+                LabelTextFieldNumberView(input: self.$dataModel.adjustit, title: "IT", systemImage: "doc.plaintext")
+                    .onChange(of: self.dataModel.adjustit, perform: { newline in
+                        print("change:\(newline)")
+                        self.dataModel.adjustcalculation()
+                    })
+            }
+        }
+    }
+}
+
+struct PersonView: View {
+    @EnvironmentObject var dataModel: DataModel
+
+    var body: some View {
+        VStack {
+            HStack {
+                Label("Person", systemImage: "person.2")
+                    .font(.largeTitle)
+                Spacer()
+                if self.dataModel.manmonth > self.dataModel.personsum {
+                    Text(String.init(format: "Sum:%.2f", self.dataModel.personsum))
+                        .font(.largeTitle)
+                        .foregroundColor(.green)
+                }
+                else {
+                    Text(String.init(format: "Sum:%.2f", self.dataModel.personsum))
+                        .font(.largeTitle)
+                        .foregroundColor(.red)
+                }
+            }
+            HStack {
+                LabelTextFieldNumberView(input: self.$dataModel.personfd, title: "FD", systemImage: "doc.plaintext")
+                    .onChange(of: self.dataModel.personfd, perform: { newline in
+                        print("change:\(newline)")
+                        self.dataModel.personcalculation()
+                    })
+                LabelTextFieldNumberView(input: self.$dataModel.persondd, title: "DD", systemImage: "doc.plaintext")
+                    .onChange(of: self.dataModel.persondd, perform: { newline in
+                        print("change:\(newline)")
+                        self.dataModel.personcalculation()
+                    })
+                LabelTextFieldNumberView(input: self.$dataModel.personcd, title: "CD", systemImage: "doc.plaintext")
+                    .onChange(of: self.dataModel.personcd, perform: { newline in
+                        print("change:\(newline)")
+                        self.dataModel.personcalculation()
+                    })
+                LabelTextFieldNumberView(input: self.$dataModel.personut, title: "UT", systemImage: "doc.plaintext")
+                    .onChange(of: self.dataModel.personut, perform: { newline in
+                        print("change:\(newline)")
+                        self.dataModel.personcalculation()
+                    })
+                LabelTextFieldNumberView(input: self.$dataModel.personit, title: "IT", systemImage: "doc.plaintext")
+                    .onChange(of: self.dataModel.personit, perform: { newline in
+                        print("change:\(newline)")
+                        self.dataModel.personcalculation()
+                    })
+            }
+        }
     }
 }
